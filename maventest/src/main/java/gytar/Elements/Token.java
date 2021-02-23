eee6a7fedfdb8a752fa1d8d61b0b6b67596e0060
gytar <perrotguillaume@protonmail.fr>
2021-02-23 15:27:18 +0100

    interface

--- maventest/src/main/java/gytar/Elements/Token.java
+++ maventest/src/main/java/gytar/Elements/Token.java
@@ -1,9 +1,13 @@
 package gytar.Elements;
 
-public class Token {
-    private String color; 
-    private int num; 
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
+public class Token {
+    private String color;
+    private int num;
+    private Image image;
     
     public String getColor() {
         return color;
@@ -20,11 +24,21 @@ public class Token {
     public void initYellowToken(){
         this.color = "Y";
         this.num = 0; 
+        ImageIcon ii = new ImageIcon("Elements/ressources/yellowToken.png");
+        image = ii.getImage(); 
+        
     }
+
+    public Image getImage() {
+        return image;
+    }
+    
 
     public void initRedToken(){
         this.color = "R";
         this.num = 1; 
+        ImageIcon ii = new ImageIcon("Elements/ressources/redToken.png");
+        image = ii.getImage(); 
     }
 
     public void printToken(){
--- maventest/src/main/java/gytar/Interface/Board.java
+++ maventest/src/main/java/gytar/Interface/Board.java
@@ -1,6 +1,5 @@
 package gytar.Interface;
 
-
 import gytar.DataBase.DataBase;
 import gytar.Elements.*;
 
@@ -12,6 +11,7 @@ import java.awt.*;
 public class Board extends JFrame implements ActionListener {
 
     DataBase data; 
+    Token red; 
 
     private static final long serialVersionUID = 4746733928727453745L;
 
@@ -23,6 +23,7 @@ public class Board extends JFrame implements ActionListener {
         getContentPane().setBackground(new Color(210, 210, 210));
         setSize(new Dimension(1000, 700));
         JLabel theMessage = new JLabel(message);
+        theMessage.setFont(new Font("Confortaa", Font.PLAIN, 20));
         JPanel theGrid = new JPanel(new GridLayout(6, 7));
         theGrid.setPreferredSize(new Dimension(800, 500));
         theGrid.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
@@ -35,10 +36,10 @@ public class Board extends JFrame implements ActionListener {
         
         JPanel contentPane = new JPanel(new GridBagLayout());
 
-        GridBagConstraints gbc = new GridBagConstraints(); 
+        GridBagConstraints gbc = new GridBagConstraints();
         
-        gbc.gridx = 0; 
-        gbc.gridy = 0; 
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         contentPane.add(theMessage, gbc);
 
         gbc.gridx = 0;
@@ -47,7 +48,16 @@ public class Board extends JFrame implements ActionListener {
          
         add(contentPane); 
 
-        setTitle("falling down");
+
+        red = new Token(); 
+        red.initRedToken();
+        int w = red.getImage().getWidth(this);
+        int h = red.getImage().getHeight(this); 
+        setPreferredSize(new Dimension(w, h));
+
+
+
+        setTitle("Board");
         setSize(1000, 700); 
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,6 +69,11 @@ public class Board extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent arg0) {
 
+    }
+
+    
+    public void paintComponent(Graphics g) {
+        g.drawImage(red.getImage(), 0, 0, null);
     }
     
 }
